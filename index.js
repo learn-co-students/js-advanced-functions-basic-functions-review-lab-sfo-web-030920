ca69b392838600c801f643797b4a9950f4eacdf5
Edward Mendoza <lalo.garibo90@gmail.com>
2020-04-27 14:04:15 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(adjective = "*") {
+  return function (text) {
+    if (adjective === "||") {
+      return `You are ||${text}||!`;
+    }
+    return `You are *${text}*!`;
+  };
+}
+
+let Calculator = {
+  add: function (num1, num2) {
+    return num1 + num2;
+  },
+  subtract: function (num1, num2) {
+    return num1 - num2;
+  },
+  multiply: function (num1, num2) {
+    return num1 * num2;
+  },
+  divide: function (num1, num2) {
+    return num1 / num2;
+  },
+};
+
+function actionApplyer(integer, function_array) {
+  let a = integer;
+  for (let i = 0; i < function_array.length; i++) {
+    a = function_array[i](a);
+  }
+  return a;
+}
